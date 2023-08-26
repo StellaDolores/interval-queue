806117628c307755b6bfdc8dd26fa595fd6035d8
shent <shent@123.com>
2023-08-26 17:44:17 +0800

    fix: 解决移除消息队列中,不传keys会删除队列中最末尾元素的bug; fix: 传入的keys不是数组时会移除失败的bug

--- README.md
+++ README.md
@@ -1,64 +1,71 @@
+<!--
+ * @Author: shent
+ * @Date: 2023-08-26 15:37:20
+ * @LastEditors: shent
+ * @LastEditTime: 2023-08-26 15:46:39
+ * @Description: 
+-->
 # 一个简单的全局事件循环队列
 
-## 使用方法
+## 使用方法，安装依赖
 
 ```bash
 npm i pr-interval-queue
 ```
 
-#### 按需引入
+#### 按需引入方法
 
 ```js
 import { removeQueueItem, addQueueItem, startQueue } from 'pr-interval-queue'
 ```
 
-#### main.js 中开启队列
+#### 在main.js 中开启循环队列
 
 ```js
 startQueue(1000, true) // 循环频率ms，是否开启调试
 ```
 
-#### 简单使用
+#### 简单使用示例
 
 ```js
 // 需要执行的函数
 const func = () => {}
-// 添加一个事件10000ms执行一次
+// 添加一个每10000ms执行一次的事件 func
 addQueueItem({ func, interval: 10000 })
 ```
 
-#### 指定事件 key
+#### 可指定事件key，用作以后区分或清除事件
 
 ```js
 // 需要执行的函数
 const func = () => {}
 
-// 指定事件key (重复添加相同事件会清除上一个)
-addQueueItem({ func, interval: 3000, key: 'bbb-3' })
+// 指定事件func的key (重复添加相同事件会清除上一个)
+addQueueItem({ func, interval: 3000, key: 'a-1' })
 ```
 
-#### 在未来添加一个循环事件
+#### 在未来的某个时刻添加一个事件
 
 ```js
 // 需要执行的函数
 const func = () => {}
 
-// 给一天后添加一个每6000ms的循环事件
-addQueueItem({ func, interval: 6000, key: 'ccc-6', execution_time: new Date().getTime() + 1000 * 60 * 60 * 24 * 1 })
+// 给一天后添加一个每6000ms执行一次的事件 func
+addQueueItem({ func, interval: 6000, key: 'b-1', execution_time: new Date().getTime() + 1000 * 60 * 60 * 24 * 1 })
 ```
 
-#### 移除事件
+#### 移除已添加的事件
 
 ```js
 // 需要执行的函数
 const func = () => {}
 
-// 保存添加后的key 如果不指定key，会随机生成一个并返回
+// 通过变量保存给事件fun添加的key 如果不指定key，addQueueItem()会生成一个随机key并返回
 const key = addQueueItem({ func, interval: 3000 })
 removeQueueItem([key])
 ```
 
-## 代码仓库
+## 源代码仓库
 
 [github](https://github.com/breathe97/pr-interval-queue)
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 // 时间线 从左往右 越来越小 <---------------------执行3---<---执行2---<---执行1-----当前时间
 
 const queue = [] // 事件队列 执行安装从右往左执行，右侧事件执行完成时候会删除 然后插入到左侧重新等待下一次执行
+
+// 是否开启debug模式
 let debug = false
 
-// 随机生成uuid
+/**
+ * @function uuid 通过数字和大小写字母随机生成uuid
+ * @param {Number} len 
+ * @param {Number} radix 
+ * @returns 返回随机的字符串uuid
+ */
 export const uuid = (len = 16, radix = 16) => {
   let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
   let uuid = [],
     i
   radix = radix || chars.length
   if (len) {
-    // Compact form
     for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)]
   } else {
-    // rfc4122, version 4 form
     let r
-    // rfc4122 requires these characters
     uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-'
     uuid[14] = '4'
-    // Fill in random data.  At i==19 set the high bits of clock sequence as
-    // per rfc4122, sec. 4.1.5
     for (i = 0; i < 36; i++) {
       if (!uuid[i]) {
         r = 0 | (Math.random() * 16)
@@ -30,42 +32,57 @@ export const uuid = (len = 16, radix = 16) => {
   return uuid.join('')
 }
 
-// 移除事件
+/**
+ * @function removeQueueItem 移除事件
+ * @param {string[]} keys  传入事件key的字符串数组
+ */
 export const removeQueueItem = (keys = []) => {
-  // console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:keys`, keys)
+  if (debug) console.log('\x1B[46m\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#16d46b;padding:20px 0;', `Stella ===> 传入的keys`, keys);
+  if (typeof keys === 'string') keys = [keys]
   for (const key of keys) {
+    // 找到队列中指定key的事件，找到符合条件的返回对应元素的索引，如果没找到返回-1
     let index = queue.findIndex((item) => item.key === key)
-    queue.splice(index, 1)
-    if (debug) {
-      console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:移除事件`, key)
-    }
+    // 将指定key的事件移除队列，当返回 -1 时会删除最末尾的元素，所以要增加判断
+    if (index != -1) queue.splice(index, 1)
+    if (debug) console.log('\x1B[46m\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#16d46b;padding:20px 0;', `Stella ===> 移除数组中当前的key`, key);
   }
 }
 
-// 添加事件
+/**
+ * @function addQueueItem 在消息队列中添加事件
+ * @param {FunInfo} funInfo 消息队列中添加的事件及相关信息
+ * @param {Boolean} clear 默认设置为true
+ * @returns 
+ */
 export const addQueueItem = (funInfo, clear = true) => {
   let { interval, key, func, execution_time } = funInfo || {}
-  // 通过time生成执行时间
+  // 通过时间戳生成事件的激活时间
   if (!execution_time) {
     execution_time = new Date().getTime() + Number(interval)
   }
+  // 如果没有传入自定义的key, 则拼接生成默认key
   if (!key) {
     key = `${execution_time}-${uuid(4, 16)}`
   }
-  // 主要为了 循环调用的时候不清除 因为直接获取到当前index 在内部清理 不需要查询
+  // 内部清除已存在的指定key的事件, 不需要额外查询
   if (clear) {
-    // 如果key已经存在则先清除存在的事件对象
-    const hasKey = queue.find((item) => item.key === key)
+    // 如果队列中已存在某个事件的key与传入key相同, 则先将已存在的事件移除队列
+    const hasKey = queue.find((item) => item.key === key) // 不存在返回undefined
     if (hasKey) {
-      // console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:removeQueueItem`, key)
       removeQueueItem([key])
+      if (debug) {
+        console.log('\x1B[46m\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#16d46b;padding:20px 0;', `Stella ===> 当前${key}已存在, 已将队列中重复存在的事件移除`);
+      }
     }
   }
-  // 根据execution_time 把info插入到队列
+  // 比较execution_time 把info插入到队列
   const info = { key, execution_time, interval, func }
-  let index = queue.findIndex((item) => execution_time >= item.execution_time) // 查找比当前大的时间点
-  // console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:index`, index, index2)
-  index = index === -1 ? queue.length : index // 如果是-1 表示没有比他小的 此时插入到最右侧在时间线最前面
+  // 因为时间线是逆序排列, 所以此处返回第一个比传入时间戳小的队列事件索引
+  let index = queue.findIndex((item) => item.execution_time <= execution_time)
+  console.log('\x1B[46m\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#16d46b;padding:20px 0;', `Stella ===> 第一个比传入时间戳小的index`, index);
+  // 如果是-1 表示没有比传入时间戳小的队列事件 此时需要传入事件插入到时间线最前面
+  index = index === -1 ? queue.length : index
+  // 例如, let arr = [5,4,3,2,1]  插入2.5,  获取到索引[3]  arr.splice(3,0,info) 打印结果 [5,4,3,2.5,2,1]
   queue.splice(index, 0, info) // 插入到数组中
   if (debug) {
     console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:插入事件`, key, execution_time, index)
@@ -74,29 +91,37 @@ export const addQueueItem = (funInfo, clear = true) => {
     let _arr3 = Array.from(queue, (item) => item.key)
     console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:队列keys`, JSON.stringify(_arr3))
     console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:时间线${_arr.length}`, JSON.stringify(_arr) === JSON.stringify(_arr2) ? '正常' : '异常', JSON.stringify(_arr))
-    console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:---------------------------------`)
   }
   return key
 }
 
-// 开启轮询队列监听
+/**
+ * @function startQueue 在main文件中开启轮询队列监听
+ * @param {Number} interval 多少秒后开启全局消息队列
+ * @param {Boolean} _debug 是否在开启轮询队列中的debug模式,会console.log debug提示
+ * @returns 
+ */
 export const startQueue = (interval = 1000, _debug = false) => {
   debug = _debug // 是否开启调试
   const timer = setInterval(() => {
     const now = new Date().getTime() // 当前时间
     let length = queue.length
     for (let i = length; i > 0; i--) {
+      // 取出消息队列的最末尾索引的事件,即最新需要执行的事件
       const info = queue[i - 1]
       const { interval, execution_time = 0, func, key } = info
-      // 执行事件距离当前时间大于间隔时间时表示 没有可执行的队列 直接跳出循环
+      // 最新需要执行的事件距离当前时间的差值 大于该事件的激活时间, 即表示队列中没有可执行的事件 跳出当前for循环
       const isBreak = execution_time - now > 0
-      if (debug) {
-        console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:队列循环`, isBreak, (execution_time - now) / 1000, JSON.stringify(info))
+      if (isBreak) {
+        if (debug) {
+          console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:当前队列循环被中断`, isBreak, (execution_time - now) / 1000, JSON.stringify(info))
+        }
+        break
       }
-      if (isBreak) break
-      // 需要执行当前事件
+      // 执行info中的事件
       func()
-      // 执行完了之后 把当前事件移动到队列最左侧
+      // 执行完毕后,将事件放置队列最左侧,即当前时间线的最末尾
+      // 删除当前已执行的事件
       queue.splice(i - 1, 1)
       const funInfo = { interval, key, func }
       addQueueItem(funInfo, false)
